7288eb038ad8de7d2d70e79e68f46d2245f36bbd
Kyle Weber <kyle.weber@uwaterloo.ca>
2020-04-09 21:14:50 -0400

    Added two-way mixed ANOVA (group x model)

--- MultisubjectProcessing.py
+++ MultisubjectProcessing.py
@@ -9,8 +9,8 @@ import numpy as np
 import scipy
 import os
 import seaborn as sns
+import researchpy as rp
 import statsmodels.api as sm
-from statsmodels.formula.api import ols
 import statsmodels.stats.multicomp
 import pingouin as pg
 
@@ -704,7 +704,7 @@ class AverageAnkleCountsValidInvalid:
         plt.legend()
 
 
-class RelativeActivityEffect:
+class RelativeActivityEffectDiff:
 
     def __init__(self, data_file):
         """Statistical analysis between relatively active and inactive groups for between-model differences in
@@ -717,13 +717,37 @@ class RelativeActivityEffect:
         self.low_active_ids = [3026, 3030, 3034, 3037, 3039]
 
         self.df = None
+        self.norm_df_intensity = None
+        self.norm_df_comparison = None
 
         """RUNS METHODS"""
         self.import_data()
+        self.normalize_max_min()
 
     def import_data(self):
 
         self.df = pd.read_excel(self.data_file, index_col="ID")
+
+    def normalize_max_min(self):
+        col_names = ['SEDENTARY', 'LIGHT', 'MODERATE', 'VIGOROUS', 'SEDENTARY%', 'LIGHT%', 'MODERATE%', 'VIGOROUS%']
+
+        self.norm_df_intensity = pd.DataFrame(columns=col_names)
+        self.norm_df_comparison = pd.DataFrame(columns=col_names)
+
+        # NORMALIZED BY INTENSITY -------------------------------------------------------------------------------------
+        for subj in self.high_active_ids + self.low_active_ids:
+            temp_df = self.df.loc[self.df.index.values == subj]
+            temp_df["ID"] = [subj for i in range(6)]
+
+            norm_subj_df = pd.DataFrame(temp_df["ID"])
+
+            for col in col_names:
+                min_val = min(temp_df[col])
+                max_val = max(temp_df[col])
+
+                norm_subj_df[col] = pd.DataFrame((temp_df[col] - min_val) / (max_val - min_val))
+
+            self.norm_df_intensity = self.norm_df_intensity.append(norm_subj_df, ignore_index=True)
 
     def comparison_by_group_anova(self, dependent_var):
         """Performs a Group x Comparison mixed ANOVA on the dependent variable that is passed in.
@@ -755,10 +779,215 @@ class RelativeActivityEffect:
 
         return aov, posthoc
 
+    def group_by_intensity_anova(self, model_comparison, data_type="percent", use_normed=False):
+        """Performs a Group x Intensity mixed ANOVA on the dependent variable that is passed in.
+           Performs pairwise T-test comparisons for post-hoc analysis.
+           Plots group means using Seaborn package.
+
+        :argument
+        -model_comparison: name of column in self.df to use as dependent variable
+        -data_types: 'minutes' or 'percent'; type of data to use
+        -use_norm: whether or not to use normed data
+
+        :returns
+        -data objects from pingouin ANOVA and posthoc objects
+        """
+
+        # DATA FORMATTING ---------------------------------------------------------------------------------------------
+        if use_normed:
+            df = self.norm_df
+        if not use_normed:
+            df = self.df
+
+        # Pulls rows from self.df for desired model comparison
+        comp_names = ["Wrist-Ankle", "Wrist-HR", "Wrist-HRAcc", "Ankle-HR", "Ankle-HRAcc", "HR-HRAcc"]
+        row_int = comp_names.index(model_comparison)
+        df2 = df.iloc[0::6]
+
+        # df for minutes data
+        mins_df = df2[["SEDENTARY", "LIGHT", "MODERATE", "VIGOROUS"]]
+
+        # df for % data
+        perc_df = df2[["SEDENTARY%", "LIGHT%", "MODERATE%", "VIGOROUS%"]]
+
+        # Sets df to correct data type
+        if data_type == "percent":
+            df = perc_df
+        if data_type == "minutes":
+            df = mins_df
+
+        df["ID"] = self.high_active_ids + self.low_active_ids
+
+        # Creates column in df of IDs
+        df_long = pd.melt(frame=df, id_vars="ID", var_name="INTENSITY", value_name="VALUE")
+
+        high_list = ["HIGH" for i in range(5)]
+        low_list = ["LOW" for i in range(5)]
+        group_list = high_list + low_list
+        df_long["GROUP"] = (group_list * 4)
+
+        print(df_long)
+
+        # DATA VISUALIZATION -----------------------------------------------------------------------------------------
+
+        # Creates 2x1 subplots of group means
+        plt.subplots(1, 2, figsize=(12, 7))
+        plt.subplots_adjust(wspace=0.20)
+        plt.suptitle("Group x Intensity Mixed ANOVA: {} "
+                     "(normalized={})".format(model_comparison.capitalize(), use_normed))
+
+        # Two activity level groups: one line for each intensity
+        plt.subplot(1, 2, 1)
+        sns.pointplot(data=df_long, x="GROUP", y="VALUE", hue="INTENSITY",
+                      dodge=False, markers='o', capsize=.1, errwidth=1, palette='Set1')
+        plt.ylabel("Difference ({})".format(data_type))
+        plt.axhline(y=0, linestyle="dashed", color='black')
+
+        # Four intensity groups: one line for each activity level group
+        plt.subplot(1, 2, 2)
+        sns.pointplot(data=df_long, x="INTENSITY", y="VALUE", hue="GROUP",
+                      dodge=False, markers='o', capsize=.1, errwidth=1, palette='Set1')
+        plt.ylabel("")
+        plt.axhline(y=0, linestyle="dashed", color='black')
+
+        # STATISTICAL ANALYSIS ---------------------------------------------------------------------------------------
+        print("\nPerforming Group x Comparison mixed ANOVA using {} data "
+              "for the {} model.".format(data_type, model_comparison))
+
+        # Group x Intensity mixed ANOVA
+        aov = pg.mixed_anova(dv="VALUE", within="INTENSITY", between="GROUP", subject="ID", data=df_long)
+        pg.print_table(aov.iloc[:, 0:8])
+        pg.print_table(aov.iloc[:, 9:])
+
+        posthoc = pg.pairwise_ttests(dv="VALUE", within="INTENSITY", between='GROUP', subject='ID', data=df_long)
+        pg.print_table(posthoc)
+
+        return aov, posthoc
 
 
-"""
-x = RelativeActivityEffect('/Users/kyleweber/Desktop/Data/OND07/Processed Data/ActivityGroupData.xlsx')
-aov, posthoc = x.comparison_by_group_anova(dependent_var="SEDENTARY%")
-"""
+# x = RelativeActivityEffectDiff('/Users/kyleweber/Desktop/Data/OND07/Processed Data/'
+#                               'Activity Level Comparison/ActivityGroupData_Differences.xlsx')
+# x.norm_df_intensity["GROUP"] = x.df["GROUP"].values
 
+
+class RelativeActivityEffect:
+
+    def __init__(self, data_file=None):
+
+        self.data_file = data_file
+        self.df_mins = None
+        self.df_percent = None
+        self.aov = None
+        self.posthoc = None
+
+        """RUNS METHODS"""
+        self.load_data()
+
+    def load_data(self):
+
+        df = pd.read_excel(self.data_file)
+
+        self.df_mins = df[["ID", 'Group', 'Model', 'Sedentary', 'Light', 'Moderate', 'Vigorous']]
+        self.df_percent = df[["ID", 'Group', 'Model', 'Sedentary%', 'Light%', 'Moderate%', 'Vigorous%']]
+
+        # self.df_mins_long = pd.melt(frame=self.df_mins, id_vars="ID", var_name="Group", value_name="Minutes")
+
+    def perform_anova(self, activity_intensity="Moderate", data_type="percent"):
+
+        if data_type == "percent":
+            df = self.df_percent
+            activity_intensity = activity_intensity + "%"
+        if data_type == "minutes":
+            df = self.df_mins
+
+        # PLOTTING ---------------------------------------------------------------------------------------------------
+        # Creates 2x1 subplots of group means
+        plt.subplots(1, 2, figsize=(12, 7))
+        plt.subplots_adjust(wspace=0.20)
+        plt.suptitle("Group x Model Mixed ANOVA: {} Activity".format(activity_intensity))
+
+        # Two activity level groups: one line for each intensity
+        plt.subplot(1, 2, 1)
+        sns.pointplot(data=df, x="Group", y=activity_intensity, hue="Model",
+                      dodge=True, markers='o', capsize=.1, errwidth=1, palette='Set1')
+        plt.ylabel("{}".format(data_type.capitalize()))
+
+        # Four intensity groups: one line for each activity level group
+        plt.subplot(1, 2, 2)
+        sns.pointplot(data=df, x="Model", y=activity_intensity, hue="Group",
+                      dodge=True, markers='o', capsize=.1, errwidth=1, palette='Set1')
+        plt.ylabel("")
+
+        # STATISTICAL ANALYSIS ---------------------------------------------------------------------------------------
+        print("\nPerforming Group x Model mixed ANOVA on {} activity.".format(activity_intensity))
+
+        # Group x Intensity mixed ANOVA
+        self.aov = pg.mixed_anova(dv=activity_intensity, within="Model", between="Group", subject="ID", data=df)
+        pg.print_table(self.aov.iloc[:, 0:8])
+        pg.print_table(self.aov.iloc[:, 9:])
+
+        group_p = self.aov.loc[self.aov["Source"] == "Group"]["p-unc"]
+        group_sig = group_p.values[0] <= 0.05
+
+        model_p = self.aov.loc[self.aov["Source"] == "Model"]["p-unc"]
+        model_sig = model_p.values[0] <= 0.05
+
+        interaction_p = self.aov.loc[self.aov["Source"] == "Interaction"]["p-unc"]
+        interaction_sig = interaction_p.values[0] <= 0.05
+
+        print("\nANOVA quick summary:")
+        if model_sig:
+            print("-Main effect of Model (p = {})".format(round(model_p.values[0], 3)))
+        if not model_sig:
+            print("-No main effect of Model")
+        if group_sig:
+            print("-Main effect of Group (p = {})".format(round(group_p.values[0], 3)))
+        if not group_sig:
+            print("-No main effect of Group")
+        if interaction_sig:
+            print("-Signficiant Group x Model interaction (p = {})".format(round(interaction_p.values[0], 3)))
+        if not interaction_sig:
+            print("-No Group x Model interaction")
+
+        self.posthoc = pg.pairwise_ttests(dv=activity_intensity, within="Model",
+                                          between='Group', subject='ID', data=df)
+
+        pg.print_table(self.posthoc)
+
+    def plot_main_effects(self, intensity):
+
+        if intensity[-1] != "%":
+            intensity += "%"
+
+        plt.subplots(3, 1, figsize=(12, 7))
+        plt.suptitle("{} Activity".format(intensity.capitalize()))
+
+        plt.subplot(1, 3, 1)
+        model_means = rp.summary_cont(self.df_percent.groupby(['Model']))[intensity.capitalize()]["Mean"]
+        model_sd = rp.summary_cont(self.df_percent.groupby(['Model']))[intensity.capitalize()]["SD"]
+        plt.bar([i for i in model_sd.index], [100 * i for i in model_means.values],
+                yerr=[i * 100 for i in model_sd], capsize=10, ecolor='black',
+                color=["Red", "Blue", "Green", "Purple"], edgecolor='black', linewidth=2)
+        plt.ylabel("% of Collection")
+        plt.title("Model Means")
+
+        plt.subplot(1, 3, 2)
+        group_means = rp.summary_cont(self.df_percent.groupby(['Group']))[intensity.capitalize()]["Mean"]
+        group_sd = rp.summary_cont(self.df_percent.groupby(['Group']))[intensity.capitalize()]["SD"]
+        plt.bar([i for i in group_means.index], [100 * i for i in group_means.values],
+                yerr=[i * 100 for i in group_sd], capsize=10, ecolor='black',
+                color=["Grey", "White"], edgecolor='black', linewidth=2)
+        plt.title("Group Means")
+
+        plt.subplot(1, 3, 3)
+        sns.pointplot(data=x.df_percent, x="Model", y=intensity.capitalize(), hue="Group",
+                      dodge=True, markers='o', capsize=.1, errwidth=1, palette='Set1')
+        plt.title("All Combination Means")
+        plt.ylabel(" ")
+
+
+x = RelativeActivityEffect('/Users/kyleweber/Desktop/Data/OND07/Processed Data/'
+                           'Activity Level Comparison/ActivityGroupsData_AllActivityMinutes.xlsx')
+x.perform_anova(activity_intensity="Vigorous", data_type="percent")
+
+x.plot_main_effects("Vigorous")
